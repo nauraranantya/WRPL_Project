343148266d2b09a9b9afbbe96b66ddc3405a1448
nauraranantya <nauraranantyakinanti@gmail.com>
2025-03-18 23:05:12 +0700

    Initial commit

---
+++ backend/models/productModel.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    title: {type:String, required:true},
+    synopsis: {type:String, required:true},
+    price: {type: Number, required:true},
+    image: {type: Array, required:true},
+    genre: {type: String, required:true},
+    ageRating: {type: String, required:true},
+    sizes: {type: Array, required:true},
+    bestseller: {type:Boolean},
+    date: {type:Number, required:true}
+})
+
+const productModel = mongoose.models.product || mongoose.model("product", productSchema)
+
+export default productModel
