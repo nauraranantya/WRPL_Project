021999b22b40d470b2aa19985c57a9761c75014d
keefanimentari <keefani05@gmail.com>
2025-03-20 18:30:34 +0700

    Update punya kiffy

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -46,11 +46,31 @@ const placeOrderRazorpay = async (req,res) => {
 // All orders data for Admin Panel
 const allOrders = async (req,res) => {
 
+    try {
+        
+        const orders = await orderModel.find({})
+        res.json({success:true,orders})
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message})
+    }
+
 }
 
 // User Order Data for FrontEnd
 const userOrders = async (req,res) => {
+    try {
+        
+        const { userId } = req.body
 
+        const orders = await orderModel.find({ userId })
+        res.json({success:true,orders})
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message})
+    }
 }
 
 //update order status from Admin Panel
