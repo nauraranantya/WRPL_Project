24f57adf3d10452122e2caa1501dbdf1eb811bc3
nauraranantya <nauraranantyakinanti@gmail.com>
2025-06-08 00:30:57 +0700

    Fix nginx port configuration for Railway deployment

--- frontend/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Substitute env vars in the template to generate actual nginx config
-envsubst '${PORT}' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf
-
-# Start nginx in foreground
-nginx -g 'daemon off;'
